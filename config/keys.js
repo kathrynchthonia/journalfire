50317864343a9325611eec9fad6fecf835890142
Your NameTony <tony@wheelertechconsulting.com>
2018-09-23 17:18:27 -0500

    handled auth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://tony:tony4444@ds023523.mlab.com:23523/journalfire-dev',
+  googleClientID:'429433132774-j4umja028jkeat0hktm0r3q42bcmffj7.apps.googleusercontent.com',
+  googleClientSecret:'YTEf4jR5sSld6EmbvRCySRSZ'
+}
