85264724a485cacaf93ecb9345bb78a0ae7d2c7a
Your NameTony <tony@wheelertechconsulting.com>
2018-09-23 15:40:29 -0500

    initial commit2

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(It Works!);
+  res.send('It Works!');
 })
 
 const port = process.env.PORT || 5000;
