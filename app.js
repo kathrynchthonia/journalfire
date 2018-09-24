fa8266780513dd76b1b3f7362148c195c5995eec
Your NameTony <tony@wheelertechconsulting.com>
2018-09-24 01:43:05 -0500

    started ui

--- app.js
+++ app.js
@@ -1,23 +1,66 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const passport = require('passport');
+
+// Load user model
+require('./models/User');
 
 // Passport Config
 require('./config/passport')(passport);
 
 // Load Routes
+const index = require('./routes/index');
 const auth = require('./routes/auth');
+
+// load Keys
+const keys = require('./config/keys');
+
+// Map Global promises
+mongoose.Promise = global.Promise;
+
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, { 
+  useNewUrlParser: true 
+})
+  .then(() => console.log('Mongodb Connected...'))
+  .catch(err => console.log(err));
 
 const app = express();
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 
-app.get('/', (req, res) => {
-  res.send('It Works!');
+
+
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
+  next();
 });
 
+
 // Use Routes
+app.use('/', index);
 app.use('/auth', auth);
+
 
 const port = process.env.PORT || 5000;
 
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
+// Load User model
+const User = mongoose.model('users');
 
 module.exports = function(passport){
   passport.use(new GoogleStrategy({
@@ -10,9 +12,44 @@ module.exports = function(passport){
     proxy: true
   },
   (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    console.log(profile);
+    // console.log(accessToken);
+    // console.log(profile);
+
+    const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
+    
+    const newUser = {
+      googleID: profile.id,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      email: profile.emails[0].value,
+      image
+    }
+
+    // Check for exixting user
+    User.findOne({
+      googleID: profile.id
+    }).then(user => {
+      if(user){
+          // Return User
+        done(null, user);
+      } else {
+        // Create User
+        new User(newUser)
+          .save()
+          .then(user => done(null, user));
+
+      }
+    })
   }
 ));
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => done(null, user));
+  });
+
+
 }
 
